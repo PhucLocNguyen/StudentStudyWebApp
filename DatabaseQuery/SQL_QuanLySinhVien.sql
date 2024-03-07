7f5d57149b91a48a7a0da9a91512013c8fc2ddf3
PhucLocNguyen <loc19992003bca@gmail.com>
2024-03-07 10:15:20 +0700

    Database Query

--- DatabaseQuery/SQL_QuanLySinhVien.sql
+++ DatabaseQuery/SQL_QuanLySinhVien.sql
@@ -35,36 +35,32 @@ PRIMARY KEY(student_id,class_id)
 )
 
 CREATE TABLE "Exercises"(
-excercise_id int identity(1,1) PRIMARY KEY,
+exercise_id int identity(1,1) PRIMARY KEY,
 title nvarchar(255) not null,
 media varchar(255),
 description ntext,
 status NVARCHAR(10) CHECK (status IN ('active','inactive', 'pending')) DEFAULT 'pending',
 created_date datetime default GETDATE(),
-start_date datetime NULL CHECK (
-   (end_date > start_date OR (end_date IS NULL AND start_date IS NULL))
-   OR (end_date IS NULL AND start_date IS NOT NULL)
-),
-end_date datetime NULL CHECK (end_date > created_date OR end_date IS NULL),
-
+end_date datetime default NULL,
+start_date datetime default NULL,
 class_id int FOREIGN KEY REFERENCES Classes(class_id),
 lecturer_id int FOREIGN KEY REFERENCES Lecturers(lecturer_id)
 )
 CREATE TABLE "Do"(
-excercise_id int FOREIGN KEY REFERENCES Excercises(excercise_id),
+exercise_id int FOREIGN KEY REFERENCES Exercises(exercise_id),
 student_id int FOREIGN KEY REFERENCES Students(student_id),
 solution ntext not null,
 score float CHECK(score>=0 AND score <= 10) DEFAULT 0,
 is_grade bit default 0,
 created_date datetime default GETDATE(),
-PRIMARY KEY (excercise_id,student_id)
+PRIMARY KEY (exercise_id,student_id)
 )
 
 CREATE TABLE "Notifications"(
 notification_id int identity (1,1) PRIMARY KEY,
 message ntext not null,
 created_date datetime default GETDATE(),
-excercise_id int FOREIGN KEY REFERENCES Excercises(excercise_id),
+exercise_id int FOREIGN KEY REFERENCES Exercises(exercise_id),
 )
 
 CREATE TABLE "Receive"(
